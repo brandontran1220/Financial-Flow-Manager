481683098feec1fb4fd0ea7e4e7a8005871a593e
brandontran1220 <brandontran1220@gmail.com>
2024-09-06 17:57:35 -0700

    Added recent history, min/max from income/expense, and total from income/expense on dashboard

--- frontend/src/Components/Dashboard/Dashboard.js
+++ frontend/src/Components/Dashboard/Dashboard.js
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styled from 'styled-components'
 import { InnerLayout } from "../../styles/Layouts";
 import Chart from '../Chart/Chart'
+import { dollar } from "../../utils/Icons";
+import { useGlobalContext } from "../../context/globalContext";
+import History from '../../History/History';
 
 function Dashboard() {
+    const {totalExpenses, incomes, expenses, totalIncome, totalBalance, getIncomes, getExpenses} = useGlobalContext()
+
+    useEffect(() => {
+        getIncomes()
+        getExpenses()
+    }, [])
+
     return (
         <DashboardStyled>
             <InnerLayout>
@@ -11,6 +21,47 @@ function Dashboard() {
                 <div className="stats-con">
                     <div className="chart-con">
                         <Chart />
+                        <div className="amount-con">
+                            <div className="income">
+                                <h2>Total Income</h2>
+                                <p>
+                                    {dollar} {totalIncome()}
+                                </p>
+                            </div>
+                            <div className="expense">
+                                <h2>Total Expense</h2>
+                                <p>
+                                    {dollar} {totalExpenses()}
+                                </p>
+                            </div>
+                            <div className="balance">
+                                <h2>Total Balance</h2>
+                                <p>
+                                    {dollar} {totalBalance()}
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="history-con">
+                        <History />
+                        <h2 className="salary-title">Min <span>Income</span>Max</h2>
+                        <div className="salary-item">
+                            <p>
+                                {Math.min(...incomes.map(item => item.amount))}
+                            </p>
+                            <p>
+                                {Math.max(...incomes.map(item => item.amount))}
+                            </p>
+                        </div>
+                        <h2 className="salary-title">Min <span>Expense</span>Max</h2>
+                        <div className="salary-item">
+                            <p>
+                                {Math.min(...expenses.map(item => item.amount))}
+                            </p>
+                            <p>
+                                {Math.max(...expenses.map(item => item.amount))}
+                            </p>
+                        </div>
                     </div>
                 </div>
             </InnerLayout>
@@ -19,7 +70,35 @@ function Dashboard() {
 }
 
 const DashboardStyled = styled.div`
-
+    .stats-con{
+        display: grid;
+        grid-template-columns: repeat(5, 1fr);
+        gap: 2rem;
+        .chart-con{
+            grid-column: 1 / 4;
+            height: 400px;
+            .amount-con{
+                display: grid;
+                grid-template-columns: repeat(4, 1fr);
+                gap: 2rem;
+                margin-top: 2rem;
+                .income, .expense{
+                    grid-column: span 2;
+                }
+                .income, .expense, .balance{
+                    background: #FCF6F9;
+                    border: 2px solid #FFFFFF;
+                    box-shadow: 0px 1px 15px rgba(0, 0, 0, 0.06);
+                    border-radius: 20px;
+                    padding: 1rem;
+                    p{
+                        font-size: 3.5rem;
+                        font-weight: 700;
+                    }
+                }
+            }
+        }
+    }
 `;
 
 export default Dashboard
--- frontend/src/context/globalContext.js
+++ frontend/src/context/globalContext.js
@@ -73,6 +73,20 @@ export const GlobalProvider = ({ children }) => {
     }
 
 
+    const totalBalance = () => {
+        return totalIncome() - totalExpenses()
+    }
+
+    const transactionHistory = () => {
+        const history = [...incomes, ...expenses]
+        history.sort((a, b) => {
+            return new Date(b.createdAt) - new Date(a.createdAt)
+        })
+
+        return history
+    }
+
+
     return (
         <GlobalContext.Provider value={{ 
             addIncome,
@@ -84,7 +98,9 @@ export const GlobalProvider = ({ children }) => {
             addExpense,
             getExpenses,
             deleteExpense,
-            totalExpenses
+            totalExpenses,
+            totalBalance,
+            transactionHistory
             }}>
             {children}
         </GlobalContext.Provider>
